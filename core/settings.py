fad774d844b7b858167dbf63f329b290acaa58de
InfoIllusion <sgeorge.ug@gmail.com>
2025-07-22 12:14:26 +0300

    more changes o deploy

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = ['baptist-at-heart.onrender.com', 'localhost', '127.0.0.1']
 
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'baptist-at-heart.onrender.com',  # Add your Render URL here explicitly
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -43,13 +49,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ✅ Correct middleware for static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.storage.CompressedManifestStaticFilesStorage',
+   
     
 ]
 
@@ -119,19 +126,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 # Media files (user-uploaded files)
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # for production
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
